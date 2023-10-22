178b9a0ad84d25b216afe569be1f54edb9a4edb7
Kaarst <kaarst@ya.ru>
2023-10-23 00:12:29 +0300

    foolproof for calculator

---
+++ FoolProof.h
@@ -0,0 +1,18 @@
+#ifndef FOOLPROOF_H
+#define FOOLPROOF_H
+#include "refs.h"
+class FoolProof {
+public:
+	string equation;
+	sPtr eqPtr;
+	int GlobalReplace();
+	int IsntEmpty();
+	int NoIncorrectSymbols(bool);
+	int EqualBrackets();
+	int SignsCorrect();
+	int PointsCorrect(bool);
+	int ArgumentsExist();
+	int CorrectZeros();
+	int AllCorrect(sPtr);
+};
+#endif
