4f50678a59d92097dfbf50fd8dbce8d6de101f82
Kaarst <kaarst@ya.ru>
2023-10-23 00:13:33 +0300

    refs & libs

---
+++ refs.h
@@ -0,0 +1,23 @@
+#ifndef REFS_H
+#define REFS_H
+
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <regex>
+#include <vector>
+#include <stack>
+#include <sstream>
+#include <iomanip>
+#include <fstream>
+#include <windows.h>
+using namespace std;
+typedef string& sLink;
+typedef string* sPtr;
+int RegexFind(string s, string pattern);
+void Replace(sLink s, string pattern, string replacer);
+vector<string> SaveSplit(string str, string pattern);
+vector<string> Split(string str, string pattern);
+int Quit();
+
+#endif
